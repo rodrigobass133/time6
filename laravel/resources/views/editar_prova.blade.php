b8137ed979bf1194a7d810a9d5c49321c0d6f726
SamuellMS <samuel_melo07@hotmail.com>
2017-05-17 12:17:42 -0300

    Telas: Criação de Provas e Edição de Provas
    a antiga tela "criar_prova" passa a ser a de "editar_prova"

--- laravel/resources/views/editar_prova.blade.php
+++ laravel/resources/views/editar_prova.blade.php
@@ -5,7 +5,7 @@
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
     
-    <title>Criar Provas</title>
+    <title>Editar Provas</title>
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -86,7 +86,7 @@
   <div class="panel-group" id="conteudo">
     <div class="panel panel-default">
       <div class="panel-heading">
-        <h3 align="center"><span class="label label-primary" style="background-color: black; background-image: linear-gradient(45deg, rgba(0,0,0,0.3),  rgba(255,0,0, 0.3));">CRIAR PROVA</span></h3></div><br>
+        <h3 align="center"><span class="label label-primary" style="background-color: black; background-image: linear-gradient(45deg, rgba(0,0,0,0.3),  rgba(255,0,0, 0.3));">EDITAR PROVA</span></h3></div><br>
        
         <div class="row">
         <div class="col-md-1"></div>
@@ -113,50 +113,101 @@
       <div class="col-md-1"></div>
       <div class="col-md-2">
           <select class="form-control">
-          <option>---</option>
-          <option>ADS</option>
-          <option>Engenharia</option>
-          <option>Jogos Digitais</option>
+          <option name="turmNV3" id="turmNV3">ADS</option>
+          <option name="turmNV1" id="turmNV1">Engenharia</option>
+          <option name="turmNV2" id="turmNV2">Jogos Digitais</option>
           </select>
         </div>
         
         
         <div class="col-md-3">
           <select class="form-control">
-          <option>---</option>
-          <option>Matemática</option>
-          <option>Lógica de Programação</option>
-          <option>Programação Orientada a Objeto</option>
-          <option>Redes</option>
+            <option name="matNV1" id="matNV1">Matemática</option>
+          <option name="lpNV2" id="lpNV2">Lógica de Programação</option>
+          <option name="pooNV3" id="pooNV3">Programação Orientada a Objeto</option>
+          <option name="redNV4" id="redNV4">Redes</option>
           </select>
         </div>
 
         <div class="col-md-2">
           <select class="form-control">
-          <option>---</option>
-          <option>P1</option>
-          <option>P2</option>
-          <option>P3</option>
+          <option name="tipoNV1" id="tipoNV1">P1</option>
+          <option name="tipoNV2" id="tipoNV2">P2</option>
+          <option name="tipoNV3" id="tipoNV3">P3</option>
           </select>
         </div>
 
         <div class="col-md-2">
           <select class="form-control">
-          <option>---</option>
-          <option>ADS1</option>
-          <option>ADS2</option>
-          <option>ADS3</option>
+          <option name="turmNV1" id="turmNV1">ADS1</option>
+          <option name="turmNV2" id="turmNV2">ADS2</option>
+          <option name="turmNV3" id="turmNV3">ADS3</option>
           </select>
         </div>
-        <div class="col-md-1">
-          <a href="#"><button type="button" class="btn btn-default" style="background-color: black; background-image: linear-gradient(45deg, rgba(0,0,0,0.3),  rgba(255,0,0, 0.3));"><font color="white"><img id="icon-plus" src="icon-images/plus-icon.png" style="width: 15px; height: 15px;"> Criar  </button></a>
-        </div>
+        
       </div><br> 
+
+
+<br><div class="row">
+<div class="col-md-1"></div>
+<div class="col-md-10">
+  <!-- Table -->
+  <table class="table">
+    <tr>
+      <td colspan="4" style="background-color: black; background-image: linear-gradient(45deg, rgba(0,0,0,0.3),  rgba(255,0,0, 0.3));"><label for="exampleInputEmail1"><font color="#FFFFFF">Enunciados:</font></label></td>
+      <td colspan="1" style="background-color: black; background-image: linear-gradient(45deg, rgba(0,0,0,0.3),  rgba(255,0,0, 0.3));"><label for="exampleInputEmail1"><font color="#FFFFFF">Valor</td>
+      <td align="center" style="background-color: black; background-image: linear-gradient(45deg, rgba(0,0,0,0.3),  rgba(255,0,0, 0.3));"><label for="exampleInputEmail1"><font color="#FFFFFF">Excluir</td>
+    </tr>
+
+    <tr>
+      <td colspan="3" ><input type="checkbox" id="cbox1" value="first_checkbox"></td>
+      <td><font color="black">O que é o que é... Cabe em um olho e pesa uma tonelada?</td>
+      <td><input style="color: black;" type="textfield" align="center" name="valor" size="1" maxlength="3" value="1,0"/></td>
+      <td align="center"><button type="button" class="btn btn-default btn-lg"><img id="icon-plus" src="icon-images/delete-icon.png" style="width: 23px; height: 23px;"></button></td>
+      
+    </tr>
+    <tr>
+      <td colspan="3" ><input type="checkbox" id="cbox1" value="first_checkbox"></td>
+      <td><font color="black">O que é o que é... Cabe em um olho e pesa uma tonelada?</td>
+      <td><input style="color: black;" type="textfield" align="center" name="valor" size="1" maxlength="3" value="1,0"/></td>
+      <td align="center"><button type="button" class="btn btn-default btn-lg"><img id="icon-plus" src="icon-images/delete-icon.png" style="width: 23px; height: 23px;"></button></td>
+    </tr>
+    <tr>
+      <td colspan="3" ><input type="checkbox" id="cbox1" value="first_checkbox"></td>
+      <td><font color="black">O que é o que é... Cabe em um olho e pesa uma tonelada?</td>
+      <td><input style="color: black;" type="textfield" align="center" name="valor" size="1" maxlength="3" value="1,0"/></td>
+      <td align="center"><button type="button" class="btn btn-default btn-lg"><img id="icon-plus" src="icon-images/delete-icon.png" style="width: 23px; height: 23px;"></button></td>
+    </tr>
+    <tr>
+      <td colspan="3" ><input type="checkbox" id="cbox1" value="first_checkbox"></td>
+      <td><font color="black">O que é o que é... Cabe em um olho e pesa uma tonelada?</td>
+      <td><input style="color: black;" type="textfield" align="center" name="valor" size="1" maxlength="3" value="1,0"/></td>
+      <td align="center"><button type="button" class="btn btn-default btn-lg"><img id="icon-plus" src="icon-images/delete-icon.png" style="width: 23px; height: 23px;"></button></td>
+    </tr>
+    <tr>
+      <td colspan="3" ><input type="checkbox" id="cbox1" value="first_checkbox"></td>
+      <td><font color="black">O que é o que é... Cabe em um olho e pesa uma tonelada?</td>
+      <td><input style="color: black;" type="textfield" align="center" name="valor" size="1" maxlength="3" value="1,0"/></td>
+      <td align="center"><button type="button" class="btn btn-default btn-lg"><img id="icon-plus" src="icon-images/delete-icon.png" style="width: 23px; height: 23px;"></button></td>
+    </tr>
+    <tr>
+      <td colspan="4"></td>
+      <td><label for="exampleInputEmail1"><font color="#000000">Total:</font></label></td>
+      <td><input style="color: black;" type="textfield" align="center" name="valor" size="1" maxlength="3" value="5,0" disabled/></td>
+    </tr>
+
+  </table>
+</div></div>
+
+<div class="row">
+<div class="col-md-1"></div>
+<button type="button" class="btn btn-primary" style="background-color: black; background-image: linear-gradient(45deg, rgba(0,0,0,0.3),  rgba(255,0,0, 0.3));"> Adicionar </button>
+</div><br>
 
 <br><div class="modal-footer">
 
                 <a href="#"><button type="button" class="btn btn-default" data-dismiss="modal"> Cancelar </button></a>
-                <a href="#"> <button type="button" class="btn btn-primary" style="background-color: black; background-image: linear-gradient(45deg, rgba(0,0,0,0.3),  rgba(255,0,0, 0.3));"> Salvar </button></a>
+                <a href="#"><button type="button" class="btn btn-primary" style="background-color: black; background-image: linear-gradient(45deg, rgba(0,0,0,0.3),  rgba(255,0,0, 0.3));"> Salvar </button></a>
             </div>
 </div>
 
